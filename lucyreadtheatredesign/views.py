efb32f84ba766007a3ad233ffc0491a9bc7b296f
sjoerdk <sjoerdk1@xs4all.nl>
2016-11-12 23:47:25 +0100

    now showing featured image URL for each gallery

--- lucyreadtheatredesign/views.py
+++ lucyreadtheatredesign/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from mezzanine.galleries.models import Gallery
+from mezzanine.pages.models import Page
 
 def test(request):
     
@@ -7,5 +9,6 @@ def test(request):
 
 def homepage(request,template=u"homepage.html"):
     
-    
-    return TemplateResponse(request, template, {"skanky":"yomama"})
+    g = Gallery.objects.all()
+    p = Page.objects.all()
+    return TemplateResponse(request, template, {"skanky":"yomama","all_galleries":g})
